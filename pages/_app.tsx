78e605f0c4af5b0570f56925bd08ca8f3c59226d
Bryan Wu <bryan@xmind.org>
2025-08-18 23:58:38 +0800

    fix: type

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,14 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
+import type { Session } from "next-auth";
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({
+  Component,
+  pageProps,
+}: AppProps<{ session?: Session | null }>) {
   return (
-    <SessionProvider session={(pageProps as any)?.session}>
+    <SessionProvider session={pageProps.session}>
       <Component {...pageProps} />
     </SessionProvider>
   );
