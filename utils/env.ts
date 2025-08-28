d142179e791d268a7f877d6322fd6563d2f33af5
Bryan Wu <bryan@xmind.org>
2025-08-28 22:10:10 +0800

    chore(env): update

--- utils/env.ts
+++ utils/env.ts
@@ -4,12 +4,14 @@ const envSchema = z.object({
   OPENAI_API_KEY: z.string(),
   NEXTAUTH_URL: z.string(),
   NEXTAUTH_SECRET: z.string(),
-  SUPABASE_URL: z.string(),
-  SUPABASE_SERVICE_ROLE_KEY: z.string(),
   GOOGLE_CLIENT_ID: z.string(),
   GOOGLE_CLIENT_SECRET: z.string(),
   NODE_ENV: z.enum(["development", "production"]),
   MEM0_API_KEY: z.string(),
+  DATABASE_URL: z.string(),
+  LANGFUSE_SECRET_KEY: z.string(),
+  LANGFUSE_PUBLIC_KEY: z.string(),
+  LANGFUSE_BASEURL: z.string(),
 });
 
 let env = {} as z.infer<typeof envSchema>;
